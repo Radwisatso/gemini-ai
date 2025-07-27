542973079fc058211748a5ca8de2759077002a0a
Raditya Dwisatrio Santoso <dwisatrioraditya@gmail.com>
2025-07-27 12:14:08 +0700

    feat: scaffolding gemini project

---
+++ index.js
@@ -0,0 +1,18 @@
+import { config } from "dotenv";
+config(); // Load environment variables from .env file
+
+import { GoogleGenAI } from "@google/genai";
+// The client gets the API key from the environment variable `GEMINI_API_KEY`.
+const ai = new GoogleGenAI({
+    apiKey: process.env.GEMINI_API_KEY,
+});
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: "Explain how AI works in a few words",
+  });
+  console.log(response.text);
+}
+
+main();
